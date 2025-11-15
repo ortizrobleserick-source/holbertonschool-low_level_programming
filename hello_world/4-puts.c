365d290a5f9b70121da76f863be4cde539c1f8af
Erick Yomar <erick01726826@gmail.com>
2025-11-15 12:53:03 -0400

    Add all Hello World project files

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,7 @@
-#include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - Entry point
- *
- * Description: Prints a string using puts
  *
  * Return: Always 0 (Success)
  */
@@ -12,3 +10,4 @@ int main(void)
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
+
