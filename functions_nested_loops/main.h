97282151d174834910c837fb0515ea3cc8c70313
ortizrobleserick <ortizrobleserick@gmail.com>
2026-03-04 13:27:31 -0400

    this function will print the alphabet 10 times

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);  /* prototipo de la función */
+/* Prototipo de _putchar */
+int _putchar(char c);
+
+/* Prototipo del primer ejercicio */
+void print_alphabet(void);
+
+/* Prototipo del segundo ejercicio */
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
-
