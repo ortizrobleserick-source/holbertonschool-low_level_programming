cc781cc54759cda79e9e49cefa4b5f4c4a7f42c1
ortizrobleserick <ortizrobleserick@gmail.com>
2026-03-04 15:00:59 -0400

    functions & nested loops

--- functions_nested_loops/2-main.c
+++ functions_nested_loops/2-main.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - prueba la función print_alphabet_x10
  *
- * Return: Always 0.
+ * Return: Always 0
  */
 int main(void)
 {
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,13 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototipo de _putchar */
 int _putchar(char c);
-
-/* Prototipo del primer ejercicio */
-void print_alphabet(void);
-
-/* Prototipo del segundo ejercicio */
-void print_alphabet_x10(void);
+int _islower(int c);
 
 #endif /* MAIN_H */
