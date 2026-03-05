877299195b31b889bde14b20c192ab7d41998276
ortizrobleserick <ortizrobleserick@gmail.com>
2026-03-05 14:23:03 -0400

    Write a function that prints the sign of a number

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,7 +1,25 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Prototipo de _putchar */
 int _putchar(char c);
+
+/* Task 0: _isupper */
+int _isupper(int c);
+
+/* Task 1: _isdigit */
+int _isdigit(int c);
+
+/* Task 2: mul */
+int mul(int a, int b);
+
+/* Task 3: print_numbers */
+void print_numbers(void);
+
+/* Task 4: _isalpha */
 int _isalpha(int c);
 
-#endif
+/* Task 5: print_sign */
+int print_sign(int n);
+
+#endif /* MAIN_H */
