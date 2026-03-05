b1c147d5b660fc3e6af832ee1dd9b67a3ae2e2eb
ortizrobleserick <ortizrobleserick@gmail.com>
2026-03-05 11:06:35 -0400

    unction that checks for a digit (0 through 9)

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character
- * @c: The character to check
+ * _islower - checks if a character is lowercase
+ * @c: character to check
  *
- * Return: 1 if c is lowercase, 0 otherwise
+ * Return: 1 if lowercase, 0 otherwise
  */
 int _islower(int c)
 {
