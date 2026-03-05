82e94f099ae96ad470c65a37d6d42fe016a6658e
ortizrobleserick <ortizrobleserick@gmail.com>
2026-03-05 16:36:30 -0400

    function that prints the last digit of a number

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,28 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototype of _putchar */
 int _putchar(char c);
-
-/* Task 0: _isupper */
-int _isupper(int c);
-
-/* Task 1: _isdigit */
-int _isdigit(int c);
-
-/* Task 2: mul */
-int mul(int a, int b);
-
-/* Task 3: print_numbers */
-void print_numbers(void);
-
-/* Task 4: _isalpha */
-int _isalpha(int c);
-
-/* Task 5: print_sign */
-int print_sign(int n);
-
-/* Task 6: _abs */
-int _abs(int n);
+int print_last_digit(int n);
 
 #endif /* MAIN_H */
