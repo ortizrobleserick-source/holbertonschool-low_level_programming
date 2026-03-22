6b3c93758586fc22ff98087430bdad1514260783
ortizrobleserick <ortizrobleserick@gmail.com>
2026-03-21 23:33:46 -0400

    function that swaps the values of two integers.

--- pointers_arrays_strings/1-main.c
+++ pointers_arrays_strings/1-main.c
@@ -1,6 +1,15 @@
-int a = 98;
-int b = 42;
+/* 1-main.c para pruebas locales */
+#include "main.h"
+#include <stdio.h>  /* Solo para pruebas locales */
 
-printf("a=%d, b=%d\n", a, b);
-swap_int(&a, &b);
-printf("a=%d, b=%d\n", a, b);
+int main(void)
+{
+    int a = 98;
+    int b = 42;
+
+    printf("a=%d, b=%d\n", a, b);
+    swap_int(&a, &b);
+    printf("a=%d, b=%d\n", a, b);
+
+    return 0;
+}
