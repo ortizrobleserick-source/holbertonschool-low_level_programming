75fa391ca5a0350dbdd0590d7fc1e4e8899b1fbc
ortizrobleserick <ortizrobleserick@gmail.com>
2026-03-22 12:48:07 -0400

    function that prints a string, in reverse, followed by a new line

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,21 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Prototipos de funciones para 0 a 3 */
-
-/* 0. 98 Battery st. */
-void reset_to_98(int *n);
-
-/* 1. Don't swap horses in crossing a stream */
-void swap_int(int *a, int *b);
-
-/* 2. This report, by its very length, defends itself against the risk of being read */
-int _strlen(char *s);
-
-/* 3. I do not fear computers. I fear the lack of them */
-void _puts(char *str);
-
-/* Prototipo de _putchar */
 int _putchar(char c);
+void print_rev(char *s);
 
-#endif /* MAIN_H */
+#endif
