050d800b3d165e269aa4aa5bcb537f0de687013f
ortizrobleserick <ortizrobleserick@gmail.com>
2026-03-22 12:30:11 -0400

    Po  inter Arrays and Strings

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,16 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* 
- * File: main.h
- * Description: Header file containing prototypes for all functions
- *              used in the pointers, arrays, and strings project.
- */
+/* Prototipos de funciones para 0 a 3 */
 
-/* Function prototypes */
-int _putchar(char c);
+/* 0. 98 Battery st. */
 void reset_to_98(int *n);
+
+/* 1. Don't swap horses in crossing a stream */
 void swap_int(int *a, int *b);
+
+/* 2. This report, by its very length, defends itself against the risk of being read */
 int _strlen(char *s);
+
+/* 3. I do not fear computers. I fear the lack of them */
+void _puts(char *str);
+
+/* Prototipo de _putchar */
+int _putchar(char c);
 
 #endif /* MAIN_H */
