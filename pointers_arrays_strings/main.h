72553f9a8687b97663a6eb5f034f94de8de4fab6
ortizrobleserick <ortizrobleserick@gmail.com>
2026-03-22 17:00:49 -0400

    function that prints half of a string, followed by a new line

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,9 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c); /* prototipo de _putchar */
-
-void rev_string(char *s); /* si usas rev_string */
-void puts2(char *str);    /* prototipo de puts2 */
+void _putchar(char c);
+void puts_half(char *str);
 
 #endif /* MAIN_H */
